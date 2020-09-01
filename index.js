6623cd0ed9d0331ce5714e7b9eedbafde89cbce6
Armando Estrada <libryum33@hotmail.com>
2020-09-01 00:13:51 -0700

    modified javscript for generating template literal.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+//const axios = require('axios');
 const util = require("util");
 
 const writeFileAsync= util.promisify(fs.writeFile);
 
-// turn the inquier prompt into a function
+// turn the inquier prompt into a function with a return
 
-inquirer
-  .prompt([
+function userInput() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: 'What is the title of your project?'
+  },
     {
         type: "input", 
-        message: "Please ener a description of your project?",
+        message: "Please enter a description of your project?",
         name: "description"
       },
       {
@@ -34,7 +40,7 @@ inquirer
         name: "test"
       },
       {
-        type: "checkbox",
+        type: "list",
         message: "Please select a license to use.",
         choices: [
           "GNU GPlv3",
@@ -54,13 +60,64 @@ inquirer
         type: "input",
         message: "What is your email address?",
         name: "email"
-      },
-    ])
-    .then(function(response) {
-        console.log(response)
-    })
+      }
+  ]);
+}
+    //]).then(function(response) {
+      //console.log(response);
+   // })
+    
+  
+// function to use template literal. created a README.md template 
+  function createReadMe(answers) {
+  return ` 
+## ${answers.title}
 
+## Introduction:
+${answers.description}
+
+## Table of Contents:
+
+
+## Installation:
+${answers.instillation}
+
+## Usage:
+${answers.usage}
+
+## Contributing:
+${answers.contributors}
+
+## License:
+${answers.license}
+
+
+## Tests:
+${answers.test}
+
+## Questions:
+[GitHub Profile](https://github.com/${answers.username})
+
+For further inquiry's that pertain to this application, 
+please reach me at ${answers.email}.
+`;
+}  
+  async function initiate() {
+    try {
+      const answers = await userInput();
+      const readMeFile = createReadMe(answers);
+
+      await writeFileAsync("README.md",readMeFile);
+      console.log("It worked! Yay!")
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
+
+  initiate();
+ // console.log(userInput);
     // set up readme.md template using template literals. Then turn into a function
-    const readMe = ``
+    //const readMe = ```
 
     // create a writefile with the function above and write a readme file to add. 
